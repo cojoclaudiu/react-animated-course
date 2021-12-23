f1ec4212d1859c3681a909cfc78b70135b65eed6
Claudiu Cojocaru <62800983+cojoclaudiu@users.noreply.github.com>
2021-12-23 11:55:54 +0000

    first commit react-spring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
 import logo from './logo.svg';
 import './App.css';
+import Toggle from './Toggle';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="logo" />
-          <button className="menu-button">Menu</button>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const fade = useSpring({
+    from: { opacity: 0 },
+    opacity: 1
+  });
+
+  return (
+    <animated.div style={fade} className="App">
+      <header className="App-header">
+        <img src={logo} className="logo" />
+        <button className="menu-button">Menu</button>
+      </header>
+      <main>
+        <Toggle />
+      </main>
+    </animated.div>
+  );
+};
 
 export default App;
